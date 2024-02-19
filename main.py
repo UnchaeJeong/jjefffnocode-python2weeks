d5947a6b07a16d80f036783f13004b226a993f04
UnchaeJeong <dnsco987@gmail.com>
2024-02-19 05:36:46 +0000

    commit 방법 추가

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+'''
+> github commit > push 하는 방법
+
+git remote add origin https://github.com/UnchaeJeong/jjefffnocode-python2weeks.git
+git branch -M main
+git push -u origin main
+'''
+
 print("Hello world!")
 
 a = 2
 b = 3
-c = a+b
+c = a + b
 print(c)
 
 my_name = "nico"
